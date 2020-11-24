59aa4d7d5fa1a8020786ec0e0a14f2bb4dca25ca
Elbert Corniell <elberthcorniell@gmail.com>
2020-11-24 16:07:52 -0400

    Bugs fixed

--- bin/main.rb
+++ bin/main.rb
@@ -6,6 +6,16 @@ def clear
     system "clear" or system "cls"
 end
 
+def putBoard (arr)
+    puts "\nBoard:          Positions:\n\n"
+    puts "  #{arr[6]}  |  #{arr[7]}  |  #{arr[8]}     7     8     9"
+    puts "_____|_____|_____"
+    puts "  #{arr[3]}  |  #{arr[4]}  |  #{arr[5]}     4     5     6"
+    puts "_____|_____|_____"
+    puts "  #{arr[0]}  |  #{arr[1]}  |  #{arr[2]}     1     2     3"
+    puts "     |     |     "
+end
+
 clear
 puts "Welcome to Tic Tac Toe!"
 puts "Do you want to start with \"X\" or \"O\"?"
@@ -13,7 +23,11 @@ puts "Do you want to start with \"X\" or \"O\"?"
 while 1 do
     symbol = gets.chomp.upcase 
     if  symbol == "X" || symbol == "O"
-        ticTacToe = TicTacToe.new(symbol)
+        puts "Player 1 name:"
+        player1 = gets.chomp
+        puts "Player 2 name:"
+        player2 = gets.chomp
+        ticTacToe = TicTacToe.new(symbol, player1, player2)
         puts "Game started!"
         break
     else
@@ -24,29 +38,28 @@ end
 clear
 
 while 1 do
-    ticTacToe.putBoard
-    puts "Player #{ticTacToe.turn ? 1 : 2} turn"
+    putBoard ticTacToe.board
+    puts "It's #{ticTacToe.get_player_name} turn"
     puts "Choose your next movement!"
     movement = gets.chomp.to_i
     clear
-    if ticTacToe.isValid movement
+    if ticTacToe.is_valid? movement
         puts "Now, your move is displayed on the board!"
-        ticTacToe.makeMovement movement
+        ticTacToe.make_movement movement
     else
         puts "Invalid movement!!!!"
         next
     end
-    if ticTacToe.didWin
+    if ticTacToe.did_win?
         clear
-        ticTacToe.putBoard
-        puts "Player #{ticTacToe.turn ? 1 : 2} have won!"
+        putBoard ticTacToe.board
+        puts "Player #{ticTacToe.get_player_name true} have won!"
         break
     end
-    if ticTacToe.isDraw
+    if ticTacToe.is_draw?
         clear    
-        ticTacToe.putBoard
+        putBoard ticTacToe.board
         puts "We have a Draw!"
         break
     end
-    player = !player
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,73 +1,80 @@
+class Player 
+    attr_reader :name
+    attr_reader :symbol
+
+    def initialize(name, symbol)
+        @name = name
+        @symbol = symbol
+    end
+end
+
 class TicTacToe
     attr_reader :turn
+    attr_reader :board
     attr_writer :turn
 
-    def initialize(symbol)
-        @@player1 = symbol
-        @@player2 = symbol == "X" ? "O" : "X"
-        @@board = [" "," "," "," "," "," "," "," "," "]
+    def initialize(symbol, player1, player2)
+        @@player1 = Player.new(player1, symbol)
+        @@player2 = Player.new(player2, symbol == "X" ? "O" : "X")
+        @board = [" "," "," "," "," "," "," "," "," "]
         @turn = true
     end
 
-    def putBoard
-        arr = @@board
-        puts "\nBoard:          Positions:\n\n"
-        puts "  #{arr[6]}  |  #{arr[7]}  |  #{arr[8]}     7     8     9"
-        puts "_____|_____|_____"
-        puts "  #{arr[3]}  |  #{arr[4]}  |  #{arr[5]}     4     5     6"
-        puts "_____|_____|_____"
-        puts "  #{arr[0]}  |  #{arr[1]}  |  #{arr[2]}     1     2     3"
-        puts "     |     |     "
+    def is_valid?(movement = -1)
+        movement > 0 && movement < 10 && board[movement - 1] == " "
     end
 
-    def isValid(movement = -1)
-        movement > 0 && movement < 10 && @@board[movement - 1] == " "
-    end
-
-    def makeMovement(movement)
-        if isValid(movement)
-            @@board[movement - 1] = @turn ? @@player1 : @@player2 
-            @turn = !@turn
+    def make_movement(movement)
+        if is_valid?(movement)
+            board[movement - 1] = turn ? @@player1.symbol : @@player2.symbol
+            @turn = !turn
             return true
         end
         false
     end
 
-    def isDraw
-        availablePositions = @@board.any? " "
+    def is_draw?
+        availablePositions = board.any? " "
         if availablePositions
             return false
         end
         true
     end
 
-    def didWin
-        symbol = !@turn ? @@player1 : @@player2
+    def did_win?
+        symbol = !turn ? @@player1.symbol : @@player2.symbol
         # Check rows
-        puts symbol
         for i in 0..2 do
-            if @@board[0 + (3 * i)] == symbol && @@board[1 + (3 * i)] == symbol && @@board[2 + (3 * i)] == symbol
+            if board[0 + (3 * i)] == symbol && board[1 + (3 * i)] == symbol && board[2 + (3 * i)] == symbol
                 return true
             end
         end
         
         # Check columns
         for i in 0..2 do
-            if @@board[0 + i] == symbol && @@board[3 + i] == symbol && @@board[6 + i] == symbol
+            if board[0 + i] == symbol && board[3 + i] == symbol && board[6 + i] == symbol
                 return true
             end
         end
 
         # check diagonals
-        if @@board[0] == symbol && @@board[4] == symbol && @@board[8] == symbol
+        if board[0] == symbol && board[4] == symbol && board[8] == symbol
             return true
         end
 
-        if @@board[2] == symbol && @@board[4] == symbol && @@board[6] == symbol
+        if board[2] == symbol && board[4] == symbol && board[6] == symbol
             return true
         end
 
         false
+    end
+
+    def get_player_name(oposite = nil)
+        if oposite
+            !turn ? @@player1.name : @@player2.name
+        else
+            turn ? @@player1.name : @@player2.name
+        end
     end
 
 end
