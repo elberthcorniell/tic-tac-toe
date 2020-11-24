4ed0916f979e1108feb093bc9a44205a58da5aeb
Elbert Corniell <elberthcorniell@gmail.com>
2020-11-24 12:32:02 -0400

    isValid, makeMovement and isDraw methods created

--- bin/main.rb
+++ bin/main.rb
@@ -1,23 +1,10 @@
 #!/usr/bin/env ruby
-def putBoard(arr = [])
-    puts "\nBoard:          Positions:\n\n"
-    puts "  #{arr[0]}  |  #{arr[1]}  |  #{arr[2]}     1     2     3"
-    puts "_____|_____|_____"
-    puts "  #{arr[3]}  |  #{arr[4]}  |  #{arr[5]}     4     5     6"
-    puts "_____|_____|_____"
-    puts "  #{arr[6]}  |  #{arr[7]}  |  #{arr[8]}     7     8     9"
-    puts "     |     |     "
-end
+include './lib/game.rb'
 
+ticTacToe = nil
 def clear
     system "clear" or system "cls"
 end
-
-player = true
-board = [" "," "," "," "," "," "," "," "," ",]
-symbol = nil
-draw = false
-isValid = true
 
 clear
 puts "Welcome to Tic Tac Toe!"
@@ -26,6 +13,7 @@ puts "Do you want to start with \"X\" or \"O\"?"
 while 1 do
     symbol = gets.chomp.upcase 
     if  symbol == "X" || symbol == "O"
+        ticTacToe = TicTacToe.new(symbol)
         puts "Game started!"
         break
     else
@@ -36,32 +24,29 @@ end
 clear
 
 while 1 do
-    putBoard board
-    puts "Player #{player ? 1 : 2} turn"
+    ticTacToe.putBoard
+    puts "Player #{ticTacToe.turn ? 1 : 2} turn"
     puts "Choose your next movement!"
     movement = gets.chomp.to_i
     clear
-    if isValid # movement > 0 && movement < 10 && board[movement] == " " # We need to check if the position is available too
+    if ticTacToe.isValid movement # We need to check if the position is available too
         puts "Now, your move is displayed on the board!"
-        #board[movement] = symbol == "X" ? player ? "X" : "O" : player ? "X" : "O"
+        ticTacToe.makeMovement movement
     else
-        isValid = true
         puts "Invalid movement!!!!"
         next
     end
-    if draw # We are going to check here if theres a draw
-        clear
-        putBoard board
-        puts "Player #{player ? 1 : 2} move is a draw!"
-        break
-    end
     if false # We are going to check here if someone has won
         clear
-        putBoard board
-        puts "Player #{player ? 1 : 2} have won!"
+        ticTacToe.putBoard
+        puts "Player #{ticTacToe.turn ? 1 : 2} have won!"
         break
     end
-    isValid = false
-    draw = true
+    if ticTacToe.draw # We are going to check here if theres a draw
+        clear    
+        ticTacToe.putBoard
+        puts "Player #{ticTacToe.turn ? 1 : 2} move is a draw!"
+        break
+    end
     player = !player
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,43 @@
 class TicTacToe
+    attr_reader :turn
+
     def initialize(symbol)
-        
+        @@player1 = symbol
+        @@player2 = symbol == "X" ? "O" : "X"
+        @@board = [" "," "," "," "," "," "," "," "," ",]
+        @@turn = true
     end
+
+    def putBoard
+        arr = @@board
+        puts "\nBoard:          Positions:\n\n"
+        puts "  #{arr[0]}  |  #{arr[1]}  |  #{arr[2]}     1     2     3"
+        puts "_____|_____|_____"
+        puts "  #{arr[3]}  |  #{arr[4]}  |  #{arr[5]}     4     5     6"
+        puts "_____|_____|_____"
+        puts "  #{arr[6]}  |  #{arr[7]}  |  #{arr[8]}     7     8     9"
+        puts "     |     |     "
+    end
+
+    def isValid(movement = -1)
+        movement > 0 && movement < 10 && board[movement] == " "
+    end
+
+    def makeMovement(movement)
+        if isValid(movement)
+            @@board[movement - 1] = @@turn ? @@player1 : @@player2 
+            @@turn = !@@turn
+            return true
+        end
+        false
+    end
+
+    def isDraw
+        availablePositions = @@board.any? " "
+        if availablePositions
+            return false
+        end
+        true
+    end
+
 end
