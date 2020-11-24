7a25aa9cf7399f3ae20c5b770ab47d9afd00a4e4
Elbert Corniell <elberthcorniell@gmail.com>
2020-11-24 11:30:25 -0400

    Changes added

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,26 @@
 #!/usr/bin/env ruby
 
-def putBoard
+def putBoard(arr = [])
     puts "\nBoard:          Positions:\n\n"
-    puts "___|___|___     1   2   3"
-    puts "___|___|___     4   5   6"
-    puts "   |   |        7   8   9"
+    puts "  #{arr[0]}  |  #{arr[1]}  |  #{arr[2]}     1     2     3"
+    puts "_____|_____|_____"
+    puts "  #{arr[3]}  |  #{arr[4]}  |  #{arr[5]}     4     5     6"
+    puts "_____|_____|_____"
+    puts "  #{arr[6]}  |  #{arr[7]}  |  #{arr[8]}     7     8     9"
+    puts "     |     |     "
 end
 
 def clear
     system "clear" or system "cls"
 end
 
+player = true
+board = [" "," "," "," "," "," "," "," "," ",]
+symbol = nil
+draw = false
+isValid = true
+
+clear
 puts "Welcome to Tic Tac Toe!"
 puts "Do you want to start with \"X\" or \"O\"?"
 
@@ -27,23 +37,34 @@ end
 clear
 
 while 1 do
-    putBoard
-    puts "Turn"
+    putBoard board
+    puts "Player #{player ? 1 : 2} turn"
     puts "Choose your next movement!"
     movement = gets.chomp.to_i
-    if movement > 0 && movement < 10 # We need to check if the position is available too
+    clear
+    if isValid # movement > 0 && movement < 10 && board[movement] == " " # We need to check if the position is available too
         puts "Now, your move is displayed on the board!"
-        #next
+        #board[movement] = symbol == "X" ? player ? "X" : "O" : player ? "X" : "O"
     else
-        clear
+        isValid = true
         puts "Invalid movement!!!!"
         next
     end
-    if 1 # We are going to check here if someone has won
+    if draw # We are going to check here if theres a draw
         clear
-        puts "You have won!"
+        putBoard board
+        puts "Player #{player ? 1 : 2} move is a draw!"
         break
     end
+    if false # We are going to check here if someone has won
+        clear
+        putBoard board
+        puts "Player #{player ? 1 : 2} have won!"
+        break
+    end
+    isValid = false
+    draw = true
+    player = !player
 end
 
 
